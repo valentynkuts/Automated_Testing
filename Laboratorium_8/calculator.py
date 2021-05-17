2f9496bddba1da17fe3ca114f4d89da612fc865e
valentynkuts <valik.kuts@gmail.com>
2021-05-17 20:45:34 +0200

    lab8

---
+++ Laboratorium_8/calculator.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+
+
+class FuelTest:
+    url = 'https://www.calculator.net/fuel-cost-calculator.html'
+    driver = webdriver
+
+    def __init__(self, webdriver):
+        self.driver = webdriver
+
+    def open(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+
+    def CalculateCost(self):
+        distance = self.driver.find_element_by_xpath('//*[@id ="content"]/table/tbody/tr[1]/td[2]/input')
+        distance.clear()
+        distance.send_keys("300")
+        efficiency = self.driver.find_element_by_xpath(' //*[@id ="content"]/table/tbody/tr[2]/td[2]/input')
+        efficiency.clear()
+        efficiency.send_keys("7.5")
+        price = self.driver.find_element_by_xpath('//*[@id ="content"]/table/tbody/tr[3]/td[2]/input')
+        price.clear()
+        price.send_keys("2.8")
+        calculate = self.driver.find_element_by_xpath('//*[@id="content"]/table/tbody/tr[4]/td/input')
+        calculate.click()
+
+    def getMessage(self):
+        info = self.driver.find_element_by_xpath('//*[@id="content"]/p[2]')
+        message = info.text
+        return message
+
+    def close(self):
+        self.driver.close()
+
+
+# test
+driver = webdriver.Chrome(executable_path='webdriver/chromedriver')
+f = FuelTest(driver)
+f.open()
+f.CalculateCost()
+message = f.getMessage()
+assert message == "This trip will require 22.5 liters of fuel, which amounts to a fuel cost of $63."
+
+# f.close()
---
+++ Laboratorium_8/phptravels.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+import time
+
+
+class Phptravels:
+    url = 'https://phptravels.com/demo/'
+    driver = webdriver
+
+    def __init__(self, webdriver):
+        self.driver = webdriver
+
+    def open(self):
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+
+    def clickLink1(self):
+        res = self.driver.find_element_by_xpath('/html/body/div[2]/main/section[1]/div/div/div[4]/div/div/div['
+                                                '2]/div/div/div[1]/div/a/small')
+        res.click()
+
+    def clickLink(self, timeout=3):
+        start = time.monotonic()
+        end = start + timeout
+
+        while end >= time.monotonic():
+            try:
+                res = self.driver.find_element_by_xpath('/html/body/div[2]/main/section[1]/div/div/div[4]/div/div/div['
+                                                        '2]/div/div/div[1]/div/a/small')
+                if res:
+                    print(res)
+                    res.click()
+                    return 1
+
+            except:
+                print("An exception occurred")
+
+    def fillForm(self):
+        self.driver.get("https://www.phptravels.net/supplier")
+        r = self.driver.find_element_by_xpath("/html/body/div[2]/form[1]/div[1]/label[1]/input")
+        r.clear()
+        r.send_keys("supplier@phptravels.com")
+        r = self.driver.find_element_by_xpath("/html/body/div[2]/form[1]/div[1]/label[2]/input")
+        r.clear()
+        r.send_keys("demosupplier")
+        self.driver.find_element_by_xpath('/html/body/div[2]/form[1]/button').click()
+
+    def close(self):
+        self.driver.close()
+
+
+# test
+driver = webdriver.Chrome(executable_path='webdriver/chromedriver')
+p = Phptravels(driver)
+p.open()
+p.clickLink(5)
+p.fillForm()
