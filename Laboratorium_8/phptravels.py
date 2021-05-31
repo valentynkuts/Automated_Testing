e38cc70ba34d6095a58d70b47149f0acb35c4f66
valentynkuts <valik.kuts@gmail.com>
2021-05-31 20:02:10 +0200

    lab8

--- Laboratorium_8/phptravels.py
+++ Laboratorium_8/phptravels.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import time
+from selenium.webdriver import ActionChains
 
 
 class Phptravels:
@@ -18,27 +19,46 @@ class Phptravels:
                                                 '2]/div/div/div[1]/div/a/small')
         res.click()
 
-    # function waits for some time while the element is loaded
-    # when it is found it immediately executes
-    def clickLink(self, timeout=3):
+    def waitUntilLoaded(self, timeout, xpath):
         start = time.monotonic()
         end = start + timeout
 
         while end >= time.monotonic():
             try:
-                res = self.driver.find_element_by_xpath('/html/body/div[2]/main/section[1]/div/div/div[4]/div/div/div['
-                                                        '2]/div/div/div[1]/div/a/small')
+                res = self.driver.find_element_by_xpath(xpath)
                 if res:
-                    print(res)
                     res.click()
-                    return 1
+                    return res
 
             except:
-                print("An exception occurred")
+                print("Exception waitUntilLoaded")
+
+    def waitUntilLoadedDropDownList(self, timeout, xpath):
+        start = time.monotonic()
+        end = start + timeout
+
+        while end >= time.monotonic():
+            try:
+                res = self.driver.find_element_by_xpath(xpath)
+                if res:
+                    action = ActionChains(self.driver)
+                    action.move_to_element(res).perform()
+                    res.click()
+                    return res
+
+            except:
+                print("Exception waitUntilLoadedDropDownList")
+
+    # function waits for some time while the element is loaded
+    # when it is found it immediately executes
+    def clickLink(self, timeout=3):
+        self.waitUntilLoaded(timeout, '/html/body/div[2]/main/section[1]/div/div/div[4]/div/div/div[2]/div/div/div['
+                                      '1]/div/a/small')
 
     def fillForm(self):
         # switch to new opened tab
-        self.driver.switch_to_window(self.driver.window_handles[1])
+        # self.driver.switch_to_window(self.driver.window_handles[1])
+        self.driver.switch_to.window(self.driver.window_handles[1])
         r = self.driver.find_element_by_xpath("/html/body/div[2]/form[1]/div[1]/label[1]/input")
         r.clear()
         r.send_keys("supplier@phptravels.com")
@@ -47,8 +67,37 @@ class Phptravels:
         r.send_keys("demosupplier")
         self.driver.find_element_by_xpath('/html/body/div[2]/form[1]/button').click()
 
+    # HOTELS
+    def findLinkHotels(self, timeout=10):
+        self.waitUntilLoaded(timeout, '/html/body/div[3]/nav/div[2]/ul/li[2]/a')
+
+    def clickAddRoom(self, timeout=10):
+        self.waitUntilLoadedDropDownList(timeout, '/html/body/div[3]/nav/div[2]/ul/li[2]/ul/li[3]/a')
+
     def close(self):
         self.driver.close()
+
+    def findLinkLocation(self, timeout=10):
+        self.waitUntilLoaded(timeout, '//*[@id="social-sidebar-menu"]/li[3]/a')
+
+    def clickLocation(self, timeout=10):
+        self.waitUntilLoadedDropDownList(timeout, '//*[@id="Locations"]/li/a')
+
+    def add(self, timeout=10):
+        self.waitUntilLoaded(timeout, '//*[@id="content"]/div[2]/div[2]/a')
+
+    def addInfo(self):
+        status = self.waitUntilLoaded(10, '/html/body/div[3]/div/form/div[2]/div/div[2]/div[1]/div/select')
+        self.waitUntilLoadedDropDownList(5, '/html/body/div[3]/div/form/div[2]/div/div[2]/div[1]/div/select/option[1]')
+        price = self.waitUntilLoaded(10, '//*[@id="content"]/form/div[2]/div/div[2]/div[2]/div/strong/input')
+        # price = self.driver.find_element_by_xpath('//*[@id="content"]/form/div[2]/div/div[2]/div[2]/div/strong/input')
+        price.clear()
+        price.send_keys("88")
+
+    def getPrice(self):
+        # /html/body/div[3]/div/form/div[2]/div/div[2]/div[2]/div/strong/input
+        info = self.driver.find_element_by_xpath('/html/body/div[3]/div/form/div[2]/div/div[2]/div[2]/div/strong/input')
+        return info.text
 
 
 # test
@@ -57,4 +106,19 @@ p = Phptravels(driver)
 p.open()
 p.clickLink(10)
 p.fillForm()
+p.findLinkHotels()
+# time.sleep(4)
+p.clickAddRoom()
+p.addInfo()
+
+# price = p.getPrice()
+# time.sleep(4)
+# assert price == '88', 'Something wrong'
+# p.findLinkLocation()
+# p.clickLocation()
+# p.add()
+
 # p.close()
+
+
+#
